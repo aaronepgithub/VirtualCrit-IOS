2a69976c5cdb86f8db5f33d19e05c9c3135b8363
aaronep <aaronep@live.com>
2017-04-28 06:29:24 -0400

    Fr1

--- VirtualCrit IOS/VirtualCrit IOS/SecondViewController.swift
+++ VirtualCrit IOS/VirtualCrit IOS/SecondViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+
+    
+    func dateStringFromTimeInterval(timeInterval : TimeInterval) -> String{
+        let formater = DateFormatter()
+        //        formater.dateFormat = "HH:mm:ss.SS"
+        formater.dateFormat = "HH:mm:ss"
+        formater.timeZone = NSTimeZone(name: "GMT") as TimeZone!
+        let date = Date(timeIntervalSince1970: timeInterval)
+        return formater.string(from: date as Date)
+    }
+    
+    func dateStringFromTimeIntervalRound(timeInterval : TimeInterval) -> String{
+        let formater = DateFormatter()
+        formater.dateFormat = "mm:ss"
+        //formater.dateFormat = "HH:mm:ss"
+        formater.timeZone = NSTimeZone(name: "GMT") as TimeZone!
+        let date = Date(timeIntervalSince1970: timeInterval)
+        return formater.string(from: date as Date)
+    }
+    
     func updateUI() {
         
         lbl_speed.text = "\(String(format:"%.1f", Rounds.avg_speed))"
@@ -18,6 +38,11 @@ class SecondViewController: UIViewController {
         lbl_spd_avg.text = "\(String(format:"%.1f", Totals.avg_speed))"
         lbl_hr_avg.text = "\(String(format:"%.1f", Totals.avg_hr))"
         
+
+        
+        lbl_total_time.text = dateStringFromTimeInterval(timeInterval : Totals.durationTotal!) + " Total"
+        
+        lbl_round_time.text = dateStringFromTimeIntervalRound(timeInterval: Rounds.roundCurrentTimeElapsed!) + " Round"
     }
 
     @IBOutlet weak var btn_update: UIButton!
@@ -28,6 +53,8 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var lbl_spd_avg: UILabel!
     @IBOutlet weak var lbl_hr_avg: UILabel!
     
+    @IBOutlet weak var lbl_total_time: UILabel!
+    @IBOutlet weak var lbl_round_time: UILabel!
     
     @IBAction func action_update(_ sender: UIButton) {
         updateUI()
