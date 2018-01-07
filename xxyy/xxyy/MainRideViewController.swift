8308485bc2f15c914361a31f55b83afdbcfbce39
aaronep <aaronep@live.com>
2018-01-07 09:57:27 -0500

    WK AG

---
+++ xxyy/xxyy/MainRideViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainRideViewController.swift
+//  xxyy
+//
+//  Created by aaronep on 1/7/18.
+//  Copyright © 2018 aaronep. All rights reserved.
+//
+
+import UIKit
+
+class MainRideViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
