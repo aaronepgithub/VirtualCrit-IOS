a8bb88229d782c8258a5a4bb5557cc0b3257ff9a
AE <aaronep@live.com>
2019-12-25 13:27:38 -0500

    Cleanup

--- iVirtualCrit/Pods/Target Support Files/Pods-iVirtualCrit/Pods-iVirtualCrit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iVirtualCrit : NSObject
-@end
-@implementation PodsDummy_Pods_iVirtualCrit
-@end
--- iVirtualCrit/Pods/Target Support Files/Pods-iVirtualCrit/Pods-iVirtualCrit-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_iVirtualCritVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_iVirtualCritVersionString[];
-
